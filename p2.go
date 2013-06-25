d42f2d2b2c92b1bb6181ddbbb7b889ad46b9f925
Andrew Tongen <atongen@nerdery.com>
2013-06-25 16:18:13 -0500

    Add problem #2

--- p2.go
+++ p2.go
@@ -4,6 +4,37 @@ import (
   "fmt"
 )
 
+var p2fibMap = make(map[int]int)
+
+func p2getFib(n int) (fib int) {
+  if i, ok := p2fibMap[n]; ok {
+    fib = i
+  } else {
+    //fmt.Printf("Evaluating %d\n", n)
+    if n == 0 {
+      fib = 0
+    } else if n == 1 {
+      fib = 1
+    } else {
+      fib = p2getFib(n-1) + p2getFib(n-2)
+    }
+    p2fibMap[n] = fib
+  }
+  return
+}
+
 func runP2() {
-  fmt.Println("Problem #2 is running!")
+  sum := 0
+  for i := 2;; i++ {
+    n := p2getFib(i)
+    if n <= 4000000 {
+      if n % 2 == 0 {
+        fmt.Println(n)
+        sum += n
+      }
+    } else {
+      break
+    }
+  }
+  fmt.Printf("Sum: %d\n", sum)
 }
