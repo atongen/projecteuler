dd9b3fb86bd889ee54917d9a723ff32905712f91
Andrew Tongen <atongen@gmail.com>
2016-05-13 18:12:59 -0500

    solve 17

--- p017.go
+++ p017.go
@@ -1,17 +1,145 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+	"unicode/utf8"
+)
+
+var (
+	p017Groups = []string{
+		"",
+		"thousand",
+		"million",
+		"billion",
+		"trillion",
+		"quadrillion",
+		"sextillion",
+		"septillion",
+		"octillion",
+		"nonillion",
+		"decillion",
+	}
+
+	p017Ones = []string{
+		"",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+
+	p017Teens = []string{
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen",
+	}
+
+	p017Tens = []string{
+		"",
+		"",
+		"twenty",
+		"thirty",
+		"forty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety",
+	}
+)
 
 func p017NumberInWords(n int) string {
-	return ""
+	tens := []int{}
+	for n > 0 {
+		tens = append(tens, n%10)
+		n /= 10
+	}
+
+	result := []string{}
+
+	// tens in now in reverse order
+	for idx := 0; idx <= len(tens)/3; idx++ {
+		low := idx * 3
+		high := int(math.Min(float64(idx*3+3), float64(len(tens))))
+		group := tens[low:high]
+		if len(group) > 0 {
+			hundreds := 0
+			for i := 0; i < len(group); i++ {
+				v := group[i]
+				hundreds += v * int(math.Pow(10, float64(i)))
+			}
+			result = append([]string{p017HundredsWords(hundreds), p017Groups[idx]}, result...)
+		}
+	}
+
+	return p017CleanResult(result)
+}
+
+func p017HundredsWords(n int) string {
+	switch {
+	case 0 <= n && n < 10:
+		return p017Ones[n]
+	case 10 <= n && n < 20:
+		return p017Teens[n%10]
+	case 20 <= n && n < 100:
+		ones := p017Ones[n%10]
+		if ones == "" {
+			return p017Tens[n/10]
+		} else {
+			return p017Tens[n/10] + "-" + ones
+		}
+	case 100 <= n && n < 1000:
+		hund := p017HundredsWords(n % 100)
+		if hund == "" {
+			return p017Ones[n/100] + " hundred"
+		} else {
+			return p017Ones[n/100] + " hundred and " + hund
+		}
+	default:
+		panic(fmt.Sprintf("p017HundredsWords(%d)", n))
+	}
 }
 
 func p017NumberOfLetters(s string) int {
-	return 0
+	ns := strings.Replace(s, "-", "", -1)
+	ns = strings.Replace(ns, "-", "", -1)
+	ns = strings.Replace(ns, " ", "", -1)
+	return utf8.RuneCountInString(ns)
 }
 
 func p017SumOfNumberOfLetters(n int) int {
-	return 0
+	if n < 1 {
+		panic("Come on, dude.")
+	}
+
+	result := 0
+	for i := 1; i <= n; i++ {
+		word := p017NumberInWords(i)
+		nl := p017NumberOfLetters(word)
+		result += nl
+	}
+
+	return result
+}
+
+func p017CleanResult(result []string) string {
+	return strings.Replace(strings.TrimSpace(
+		strings.Join(result, " "),
+	), "  ", " ", -1)
 }
 
 /**
--- p017_test.go
+++ p017_test.go
@@ -12,8 +12,19 @@ func TestP017NumberInWords(t *testing.T) {
 		{3, "three"},
 		{4, "four"},
 		{5, "five"},
+		{6, "six"},
+		{7, "seven"},
+		{8, "eight"},
+		{9, "nine"},
+		{10, "ten"},
 		{342, "three hundred and forty-two"},
 		{115, "one hundred and fifteen"},
+		{900, "nine hundred"},
+		{999, "nine hundred and ninety-nine"},
+		{1000, "one thousand"},
+		{1001, "one thousand one"},
+		{2342, "two thousand three hundred and forty-two"},
+		{1234567001890, "one trillion two hundred and thirty-four billion five hundred and sixty-seven million one thousand eight hundred and ninety"},
 	} {
 		out := p017NumberInWords(tt.in)
 		if out != tt.out {
@@ -29,7 +40,7 @@ func TestP017NumberOfLetters(t *testing.T) {
 	}{
 		{"one", 3},
 		{"two", 3},
-		{"three", 3},
+		{"three", 5},
 		{"four", 4},
 		{"five", 4},
 		{"three hundred and forty-two", 23},
@@ -47,11 +58,11 @@ func TestP017SumOfNumberOfLetters(t *testing.T) {
 		in  int
 		out int
 	}{
-		{1, 3},
-		{2, 6},
-		{3, 9},
-		{4, 13},
-		{5, 17},
+		{1, 3},  // one
+		{2, 6},  // one two
+		{3, 11}, // one two three
+		{4, 15}, // one two three four
+		{5, 19}, // one two three four five
 	} {
 		out := p017SumOfNumberOfLetters(tt.in)
 		if out != tt.out {
